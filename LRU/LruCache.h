71d6d3f308f23a51df27511e714f15d5b871c5e6
Recogn1z <543257239@qq.com>
2025-10-02 19:09:34 -0700

    HashLru

--- LRU/LruCache.h
+++ LRU/LruCache.h
@@ -188,6 +188,37 @@ namespace Cache{
             std::unordered_map<Key, Value> historyValueMap_;
     };
 
-    
+    template<typename Key, typename Value> class HashLruCaches {
+        public:
+            HashLruCaches(size_t capacity, int sliceNum)
+            : capacity_(capacity)
+            , sliceNum_(sliceNum > 0 ? sliceNum : std::thread::hardware_concurrency()){
+                size_t sliceSize = std::ceil(capacity / static_cast<double>(sliceNum_));
+                for(int i = 0; i < sliceNum_; i++) {
+                    lruSliceCaches_.emplace_back(new LruCache<Key, Value>(sliceSize));
+                }
+            }
+        
+            void put(Key key, Value value) {
+                size_t sliceIndex = Hash(key)% sliceNum_;
+                lruSliceCaches_[sliceIndex]->put(key, value);
+            }
 
+            bool get(Key key, Value& value) {
+                size_t sliceIndex = Hash(key)% sliceNum_;
+                return lruSliceCaches_[sliceIndex]->get(key, value);
+            }
+
+            Value get(Key key) {
+                Value value;
+                memset(&value, 0 , sizeof(value));
+                get(key, value);
+                return value;
+            }
+
+        private:
+            size_t capacity_;
+            int sliceNum_;
+            std::vector<std::unique_ptr<LruCache<Key,Value>>> lruSliceCaches_;
+    };
 }
